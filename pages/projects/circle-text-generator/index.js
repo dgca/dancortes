a0429099f841633be86a9ed3ceff9c6ee1616458
Daniel Cortes <danielgcortes@gmail.com>
2022-03-02 00:45:01 -0700

    OG description

--- pages/projects/circle-text-generator/index.js
+++ pages/projects/circle-text-generator/index.js
@@ -5,6 +5,9 @@ export default function Post() {
   return (
     <Layout
       title="Circle Text Generator"
+      description="This circle text generator lets you easily create an image of text
+      that follows the curve of a circle. You can put an image inside the
+      text for extra swag factor."
       ogImage="project-circle-text-generator.jpg"
     >
       <CircleTextGenerator />
